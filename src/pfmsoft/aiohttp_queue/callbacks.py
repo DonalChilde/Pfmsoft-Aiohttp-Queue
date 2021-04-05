a0f0184c887621fdf7a517b1f6a87bbedbfff75b
Chad Lowe <pfmsoft.dev@gmail.com>
2021-04-05 12:08:47 -0700

    CheckForPages callback

--- src/pfmsoft/aiohttp_queue/callbacks.py
+++ src/pfmsoft/aiohttp_queue/callbacks.py
@@ -1,14 +1,22 @@
 import csv
 import json
 import logging
+import re
+from copy import deepcopy
 from pathlib import Path
 from string import Template
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Sequence, Union
 
 import aiofiles
 from aiohttp.client_reqrep import ClientResponse
 
-from pfmsoft.aiohttp_queue import AiohttpAction, AiohttpActionCallback
+from pfmsoft.aiohttp_queue import (
+    AiohttpAction,
+    AiohttpActionCallback,
+    AiohttpQueueWorkerFactory,
+)
+from pfmsoft.aiohttp_queue.aiohttp import ActionCallbacks
+from pfmsoft.aiohttp_queue.runners import queue_runner
 from pfmsoft.aiohttp_queue.utilities import combine_dictionaries, optional_object
 
 logger = logging.getLogger(__name__)
@@ -61,13 +69,15 @@ class LogRetry(AiohttpActionCallback):
 
 
 class CheckForPages(AiohttpActionCallback):
+    """Where page=<page number> is in query string, and x-pages is in response header."""
+
     def __init__(self) -> None:
         super().__init__()
 
     async def do_callback(self, caller: AiohttpAction, *args, **kwargs):
         """
         - check for pages
-        - copy action, with new page request
+        - copy action, with new page request,
         - add to list of child actions? use context variable?
         - add action to queue
 
@@ -76,6 +86,108 @@ class CheckForPages(AiohttpActionCallback):
         :param caller: [description]
         :type caller: AiohttpAction
         """
+        await self.collect_all_pages(caller, *args, **kwargs)
+
+    async def collect_all_pages(self, caller: AiohttpAction, *args, **kwargs):
+        _, _ = args, kwargs
+        page_count = self.check_for_pages(caller, *args, **kwargs)
+        if page_count is None:
+            return
+        actions = []
+        for page_number in range(2, page_count + 1):
+            actions.append(self.make_new_action(caller, page_number, *args, **kwargs))
+        await self.process_actions(caller, actions, *args, **kwargs)
+
+        caller.context["pfmsoft_page_report"] = self.build_report(
+            caller, actions, *args, **kwargs
+        )
+        self.handle_results(caller, actions, *args, **kwargs)
+
+    def build_report(
+        self, caller: AiohttpAction, actions: Sequence[AiohttpAction], *args, **kwargs
+    ):
+        _, _ = args, kwargs
+        caller_report = self.build_page_report(caller)
+        reports = []
+        reports.append(caller_report)
+        for action in actions:
+            reports.append(self.build_page_report(action))
+        return reports
+
+    def build_page_report(self, action: AiohttpAction):
+        if action.response is not None:
+            params = action.request_kwargs.get("params", {})
+            page_num = int(params.get("page", "1"))
+            if action.response.status != 200:
+                count = -1
+            else:
+                count = len(action.result)
+            action_report = {
+                "uid": str(action.uid),
+                "page": page_num,
+                "count": count,
+                "msg": f"Status: {action.response.status} msg: {action.response.reason}",
+            }
+            return action_report
+        return {}
+
+    def check_for_pages(self, caller: AiohttpAction, *args, **kwargs) -> Optional[int]:
+        _, _ = args, kwargs
+        response = caller.response
+        if response is not None:
+            pages = response.headers.get("x-pages", None)
+            if pages is not None:
+                page_int = int(pages)
+                logger.info("%s pages reported for action.uid:%s", pages, caller.uid)
+                return page_int
+        return None
+
+    def make_new_action(
+        self, caller: AiohttpAction, new_page: int, *args, **kwargs
+    ) -> AiohttpAction:
+        _, _ = args, kwargs
+        new_action = AiohttpAction(
+            method=caller.method,
+            url_template=caller.url_template,
+            url_parameters=deepcopy(caller.url_parameters),
+            retry_limit=caller.retry_limit,
+            request_kwargs=deepcopy(caller.request_kwargs),
+            name=str(caller.uid),
+            id_=new_page,
+            callbacks=ActionCallbacks(success=[ResponseContentToJson()]),
+        )
+        params = new_action.request_kwargs.get("params", {})
+        params["page"] = new_page
+        new_action.request_kwargs["params"] = params
+        logger.info(
+            "%s made action.uid=%s to get page %s of action.uid=%s",
+            self.__class__.__name__,
+            new_action.uid,
+            new_page,
+            caller.uid,
+        )
+        return new_action
+
+    async def process_actions(
+        self, caller: AiohttpAction, actions: Sequence[AiohttpAction], *args, **kwargs
+    ):
+        _, _ = args, kwargs
+        factory_count = self.worker_count(caller, actions, *args, **kwargs)
+        factories = [AiohttpQueueWorkerFactory() for _ in range(factory_count)]
+        await queue_runner(actions, factories)
+
+    def handle_results(
+        self, caller: AiohttpAction, actions: Sequence[AiohttpAction], *args, **kwargs
+    ):
+        _, _ = args, kwargs
+        for action in actions:
+            caller.result.extend(action.result)
+
+    def worker_count(
+        self, caller: AiohttpAction, actions: Sequence[AiohttpAction], *args, **kwargs
+    ) -> int:
+        _, _, _, _ = caller, actions, args, kwargs
+        return 5
 
 
 class SaveResultToFile(AiohttpActionCallback):
